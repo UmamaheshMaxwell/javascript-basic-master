58c0941204dc917831f9f9debb5817b7c9217bf5
Umamaheswararao Meka <maheshmeka16@gmail.com>
2021-01-22 10:15:42 +0530

    added new example

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
-/*===============================*/
+function display(){
+    console.log(username)
+    console.log(age)
 
-// arraySum([1,2,3,4,5]) //15
-// arraySum([1,2,3,4,5,6,7,8,9,10]) //55
-// arraySum([1,2, 3, 'uma', 4, 5]) // 15
-// arraySum([1,2, 3, 'uma', {'a' : 'uma'}, 4, 5]) // 15
-// arraySum([1,2,[[3],4], 5]) // 15
-
-function arraySum(array){
-    var total = 0;
-
-    for(var i =0; i<array.length; i++){
-        if(typeof array[i] === 'number'){
-            total += array[i]
-        }
-        else if (Array.isArray(array[i]))
-        {
-            total +=arraySum(array[i])
-        }
-    }
-    return total;
+    var username ='uma'
+    let age =38;
 }
 
-console.log(arraySum([1,2,3,4,5]))
-console.log(arraySum([1,2,3,4,5,6,7,8,9,10]))
-console.log(arraySum([1,2, 3, 'uma', 4, 5]))
-console.log(arraySum([1,2, 3, 'uma', {'a' : 'uma'}, 4, 5]))
-console.log(arraySum([1,2,[[3],4], 5]))
+display()
